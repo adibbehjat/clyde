0baa95e8f426dc92a19cb729a7b9b6e07fa326ab
Jonah Yordy <jonahyordy@gmail.com>
2018-01-30 08:57:55 -0500

    Add files via upload

---
+++ content.js
@@ -0,0 +1,10 @@
+var bckrndImg = "url(https://lh5.ggpht.com/lmue-B8Wo2Sa05MlCF2cMFlYEOJPzZDfLkVg4Gzsgsbo5-MiSrj5nA0MER8HJkqXPl4=h900)";
+function findThing(){
+	var foundElement = document.getElementsByClassName('appMount-14L89u');
+	if(!foundElement[0]) { setTimeout(findThing, 250); } 
+	else {
+		foundElement[0].style.backgroundImage = bckrndImg; 
+		foundElement[0].style.backgroundColor = "rgba(255, 0, 0, 0.15);";
+	}
+}
+findThing();
---
+++ popup.js
@@ -0,0 +1,4 @@
+function submit(){
+	var bckImg = "https://www.hdwallpapers.in/walls/beautiful_landscape-wide.jpg";
+	bckrndImg = "url(" + bckImg + ")";
+}
